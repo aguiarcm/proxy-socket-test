8b4be6022d33e706a403baf5bed9e045ba860f83
Andres Aguiar <aguiarcm@gmail.com>
2021-06-18 09:12:13 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+
+            int port = Integer.parseInt(args[0]);
+
+            ServerSocket ss = new ServerSocket(port);
+
+            for (; ; ) {
+                Socket client = ss.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                PrintWriter out = new PrintWriter(client.getOutputStream());
+
+                out.print("\r\n"); // End of headers
+
+                String line;
+                while ((line = in.readLine()) != null) {
+                    if (line.length() == 0)
+                        break;
+                    System.out.println(line + "\r\n");
+                }
+
+                out.close();
+                in.close();
+                client.close();
+            }
+        }
+          catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+}
+
